72064bb737464c21e81c5dd8ccfafbd61b16d310
Nicholas Checan <nicholas.checan@gmail.com>
2021-08-30 22:01:44 -0300

    Initial Version

---
+++ Conversions/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Conversions
+//
+//  Created by Checan Nicholas C on 30/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+}
+
